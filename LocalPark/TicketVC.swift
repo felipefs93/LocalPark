a67ae84f592a7e1f38048ce01c3f40437509ec76
Felipe Soares <soaresfelipef@gmail.com>
2016-07-10 19:31:46 -0300

    Custom alert added

--- LocalPark/TicketVC.swift
+++ LocalPark/TicketVC.swift
@@ -8,18 +8,46 @@
 
 import UIKit
 
-class TicketVC: UIViewController {
+class TicketVC: UIViewController, CustomAlertDelegate {
+    
+    
+    @IBOutlet weak var alert: CustomAlert!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        alert.delegate = self
 
-        // Do any additional setup after loading the view.
+        alert.unshow()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBAction func payTicket(sender: AnyObject) {
+        alert.setupTitle("Pagar ticket", andText: "Você deseja continuar com o pagamento do ticket?")
+        alert.show()
+    }
+    
+    
+    @IBAction func cancelTicket(sender: AnyObject) {
+        alert.setupTitle("Cancelar ticket", andText: "Você deseja continuar com o cancelamento do ticket?")
+        alert.show()
+    }
+    
+    func buttonOkPressed(){
+        print("Clicou Ok")
+    }
+    
+    func buttonCancelPressed(){
+        print("Clicou Cancelar")
+        alert.unshow()
+    }
+    
+    
     
 
 }
