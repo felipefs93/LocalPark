07e40901f7edb9ad715b559dfe88541117bffef0
Felipe Soares <soaresfelipef@gmail.com>
2016-07-11 01:21:00 -0300

    PaymentVC added

---
+++ LocalPark/PaymentVC.swift
@@ -0,0 +1,26 @@
+//
+//  PaymentVC.swift
+//  LocalPark
+//
+//  Created by Felipe Soares on 11/07/16.
+//  Copyright © 2016 Felipe Soares. All rights reserved.
+//
+
+import UIKit
+
+class PaymentVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
